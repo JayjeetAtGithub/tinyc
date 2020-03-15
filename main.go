4ecc0127145017691750eb7700a57ad850223b51
jayjeet <jc.github@rediffmail.com>
2020-03-16 01:33:18 +0530

    Fixes

--- main.go
+++ main.go
@@ -17,18 +17,18 @@ func check(err error) {
 	}
 }
 
-func set_cgroups() {
+func setCgroups() {
 	cgroups := "/sys/fs/cgroup"
 	mem := filepath.Join(cgroups, "memory")
-	os.Mkdir(filepath.Join(mem, "simple_container"), 0755)
+	os.Mkdir(filepath.Join(mem, "container"), 0755)
 
-	check(ioutil.WriteFile(filepath.Join(mem, "simple_container/memory.limit_in_bytes"),
+	check(ioutil.WriteFile(filepath.Join(mem, "container/memory.limit_in_bytes"),
 		[]byte("999424"), 0700))
-	check(ioutil.WriteFile(filepath.Join(mem, "simple_container/notify_on_release"),
+	check(ioutil.WriteFile(filepath.Join(mem, "container/notify_on_release"),
 		[]byte("1"), 0700))
 
 	pid := strconv.Itoa(os.Getpid())
-	check(ioutil.WriteFile(filepath.Join(mem, "simple_container/cgroup.procs"),
+	check(ioutil.WriteFile(filepath.Join(mem, "container/cgroup.procs"),
 		[]byte(pid), 0700))
 }
 
@@ -49,7 +49,8 @@ func run(command ...string) {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, command[0:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWPID  |
-		            syscall.CLONE_NEWNS   |
+					syscall.CLONE_NEWNS   |
+					syscall.CLONE_NEWIPC  |
                             syscall.CLONE_NEWNET  |
 		            syscall.CLONE_NEWUSER |
 			    syscall.CLONE_NEWUTS,
@@ -67,7 +68,7 @@ func run(command ...string) {
 							Size:        1,
 						},
 			    },
-	            }
+	    }
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -80,12 +81,14 @@ func run(command ...string) {
 }
 
 func child(command ...string) {
+	setCgroups()
+
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	check(syscall.Sethostname([]byte("simple_container.local")))
+	check(syscall.Sethostname([]byte("container")))
 	check(syscall.Chroot("./rootfs"))
 	check(os.Chdir("./rootfs"))
 	check(syscall.Mount("proc", "proc", "proc", 0, ""))
